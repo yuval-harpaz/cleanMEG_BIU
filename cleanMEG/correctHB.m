78a7c1c4150e8eafbd8929078d2efb09ac13e5d6
yuval <yuval@bar-ilan>
2013-12-10 14:09:40 +0200

    set the function to work for neuromag306, and cancelled detrending

--- cleanMEG/correctHB.m
+++ cleanMEG/correctHB.m
@@ -1,10 +1,18 @@
-function [cleanData,temp2e,period2,ECG,Rtopo]=correctHB(data,sRate,figOptions)%rawData,sRate,ecg)
+function [cleanData,temp2e,period2,MCG,Rtopo]=correctHB(data,sRate,figOptions,ECG,snrThr)
+
 % data is a matrix with rows for channels, raw data, not filtered.
 % sRate is sampling rate
-% figs=false;
-% if you want topoplot of HB (first and second sweeps) you have to have:
-% figOptions.label=var4Dlabel;
-% figOptions.layout='4D248.lay';
+%  - figOptions=false;
+% if you want fieldtrip topoplot of HB (first and second sweeps) you have to have:
+% figOptions.label=N by 1 cell arraay with channel names of data
+% figOptions.layout='4D248.lay' for 4D users. I recommend
+% 'neuromag306mag.lay' for neuromag users even if data includes also grads.
+%  - ECG can be ECG (useful for ctf users) or a mean of subset of MEG channels where the HB is
+% visible. neuromag users can put there the mean of the magnetometers to
+% clean both magnetometers and gradiometers included in data.
+% - nsrThr is the threshold that tell which channels are cleaned and which
+% remain as are. use 0 to clean all, default is 1.5
+%
 % 4D users can run the function from the folder with 'c,*' file with no
 % input arguments:
 % cleanData=correctHB;
@@ -13,12 +21,10 @@ function [cleanData,temp2e,period2,ECG,Rtopo]=correctHB(data,sRate,figOptions)%r
 % added by Dr. Yuval Harpaz to Prof. Abeles' work
 
 % Issues
-%  - no fail warning for data with no HB artifact
-%  - overlap between two heartbeats may have some nosie from summing two
-% templates
-%  - designed for magnetometers, needs adjustment for mag + grad data, like
-%  finding peaks using magnetometers and removing averaged template for
-%  each channel (including grad)
+%  - amplitude estimate per HB needs be better tested
+%  - only R amplitude is corrected, may consider to change q s and t waves.
+%  - memory problem, I should fix it to make topo and template based on the
+%  beginning of the data to clean the rest with it as well, piece by piece.
 %
 % it works, try it!
 
@@ -33,6 +39,9 @@ else
 end
 minPeriod=0.45;
 maxPeriod=1.5;
+if ~exist('ECG','var')
+    ECG=[];
+end
 %% checking defaults for 4D data
 % to use with data=[] and sRate=[];
 if ~exist('data','var')
@@ -48,7 +57,7 @@ if isempty(data);
     var4DnSamp=var4Dhdr.epoch_data{1,1}.pts_in_epoch;
     var4Dchi = channel_index(var4Dp, 'meg', 'name');
     data = read_data_block(var4Dp,[1 var4DnSamp],var4Dchi);
-    
+    data=double(data);
     if figs
         var4Dlabel=channel_label(var4Dp,var4Dchi)';
         figOptions.label=var4Dlabel;
@@ -56,12 +65,25 @@ if isempty(data);
     end
     clear var4D*
 end
-
+%% filter mean MEG (or ECG) data
+if ~isempty(ECG)
+    meanMEG=ECG;
+    %meanMEGdt=detrend(meanMEG,'linear',round(sRate:sRate:length(meanMEG)));
+else
+    meanMEG=mean(data);
+end
+% filtering to pass from 5-7Hz to 100-110Hz
+BandPassSpecObj=fdesign.bandpass(...
+    'Fst1,Fp1,Fp2,Fst2,Ast1,Ap,Ast2',...
+    5,7,90,110,60,1,60,sRate);
+BandPassFilt=design(BandPassSpecObj  ,'butter');
+meanMEGf = myFilt(meanMEG,BandPassFilt);
+% baseline correction again, just in case
+meanMEGf=meanMEGf-median(meanMEGf);
+sampBefore=round(sRate*maxPeriod);
 %% look for a noisy segment and noisy channels
 zThr=20;
 % find bad channels, has to be noisy for 3 of the first 3 seconds
-
-badc=zeros(size(data,1),1); % bad channels
 stdMEG=std(data(:,1:round(sRate))');
 badc=stdMEG>zThr;
 stdMEG=std(data(:,round(sRate):round(2*sRate))');
@@ -75,18 +97,14 @@ end
 
 % find jump or other huge artifact
 zThr=10;
-meanMEG=mean(data);
-meanMEGdt=detrend(meanMEG,'linear',round(sRate:sRate:length(meanMEG)));
-zMEG=(meanMEGdt-mean(meanMEGdt))./std(meanMEGdt);
+%meanMEGdt=detrend(meanMEG,'linear',round(sRate:sRate:length(meanMEG)));
+zMEG=(meanMEGf-mean(meanMEGf))./std(meanMEGf);
 jbeg=find(abs((zMEG))>zThr,1);
-bads=[]; % bad samples
+bads=[]; %#ok<NASGU> % bad samples
 if ~isempty(jbeg)
     jend=find(abs((zMEG))>zThr,1,'last');
     jend2=find(abs(zMEG(jend:end))>1,1,'last')+jend-1;
     bads=(jbeg-round(sRate./2)):(jend2+round(sRate*0.5));
-    %     if length(bads)>sRate*5
-    %         error(['jump? what''','s the noise at ',num2str(jbeg./sRate),'s ? more then 5sec between first and last noisy point, cant handle this, please clean piece by piece']);
-    %     else
     data(:,bads)=0;
     data=data-repmat(median(data(:,1:jbeg),2),1,size(data,2));
     diary('HBlog.txt')
@@ -97,30 +115,13 @@ else
     % baseline correction by removing the median of each channel
     data=data-repmat(median(data,2),1,size(data,2));
 end
+if isempty(ECG)
+    meanMEG=mean(data);
+    meanMEGf = myFilt(meanMEG,BandPassFilt);
+    meanMEGf=meanMEGf-median(meanMEGf);
+end
 
-
-
-
-%% filter mean MEG data
-
-
-
-% averaging the MEG channels
-meanMEG = mean(data);
-meanMEGdt=detrend(meanMEG,'linear',round(sRate:sRate:length(meanMEG)));
-% filtering to pass from 5-7Hz to 100-110Hz
-BandPassSpecObj=fdesign.bandpass(...
-    'Fst1,Fp1,Fp2,Fst2,Ast1,Ap,Ast2',...
-    5,7,90,110,60,1,60,sRate);
-BandPassFilt=design(BandPassSpecObj  ,'butter');
-meanMEGf = myFilt(meanMEG,BandPassFilt);
-% baseline correction again, just in case
-meanMEGf=meanMEGf-median(meanMEGf);
-
-sampBefore=round(sRate*maxPeriod);
-%% peak detection on ECG like signal
-
-
+%% peak detection on MCG (or ECG) signal
 [peaks, Ipeaks]=findPeaks(meanMEGf,1.5,round(sRate*minPeriod)); % 450ms interval minimum
 if figs
     figure;
@@ -139,19 +140,37 @@ if figs
         topo.dimord='chan_time';
         cfg=[];
         cfg.layout=figOptions.layout;
-        %cfg.interpolation='linear';
-        cfg.xlim=[1,1];
-        cfg.zlim=[-max(abs(topo.avg)) max(abs(topo.avg))];
-        figure;
-        ft_topoplotER(cfg,topo);
+        if strcmp(cfg.layout,'neuromag306mag.lay')
+            [~,magi]=ismember({'MEG0111';'MEG0121';'MEG0131';'MEG0141';'MEG0211';'MEG0221';'MEG0231';'MEG0241';'MEG0311';'MEG0321';'MEG0331';'MEG0341';'MEG0411';'MEG0421';'MEG0431';'MEG0441';'MEG0511';'MEG0521';'MEG0531';'MEG0541';'MEG0611';'MEG0621';'MEG0631';'MEG0641';'MEG0711';'MEG0721';'MEG0731';'MEG0741';'MEG0811';'MEG0821';'MEG0911';'MEG0921';'MEG0931';'MEG0941';'MEG1011';'MEG1021';'MEG1031';'MEG1041';'MEG1111';'MEG1121';'MEG1131';'MEG1141';'MEG1211';'MEG1221';'MEG1231';'MEG1241';'MEG1311';'MEG1321';'MEG1331';'MEG1341';'MEG1411';'MEG1421';'MEG1431';'MEG1441';'MEG1511';'MEG1521';'MEG1531';'MEG1541';'MEG1611';'MEG1621';'MEG1631';'MEG1641';'MEG1711';'MEG1721';'MEG1731';'MEG1741';'MEG1811';'MEG1821';'MEG1831';'MEG1841';'MEG1911';'MEG1921';'MEG1931';'MEG1941';'MEG2011';'MEG2021';'MEG2031';'MEG2041';'MEG2111';'MEG2121';'MEG2131';'MEG2141';'MEG2211';'MEG2221';'MEG2231';'MEG2241';'MEG2311';'MEG2321';'MEG2331';'MEG2341';'MEG2411';'MEG2421';'MEG2431';'MEG2441';'MEG2511';'MEG2521';'MEG2531';'MEG2541';'MEG2611';'MEG2621';'MEG2631';'MEG2641'},topo.label);
+            %topo.avg=topo.avg(chi);
+            %topo.label=topo.label(chi);
+            cfg.xlim=[1,1];
+            cfg.zlim=[-max(abs(topo.avg(magi))) max(abs(topo.avg(magi)))];
+            figure;
+            ft_topoplotER(cfg,topo);
+            title('MAGNETOMETERS, TOPOGRAPHY OF R')
+            %         cfg.layout='neuromag306planar.lay';
+            %         grd=topo.avg;
+            %         grd(chi)=0;
+            %         cfg.zlim=[-max(abs(grd)) max(abs(grd))];
+            %          figure;
+            %         ft_topoplotER(cfg,topo);
+            %         title('GRADIOMETERS')
+        else
+            %cfg.channel={'MEG0111';'MEG0121';'MEG0131';'MEG0141';'MEG0211';'MEG0221';'MEG0231';'MEG0241';'MEG0311';'MEG0321';'MEG0331';'MEG0341';'MEG0411';'MEG0421';'MEG0431';'MEG0441';'MEG0511';'MEG0521';'MEG0531';'MEG0541';'MEG0611';'MEG0621';'MEG0631';'MEG0641';'MEG0711';'MEG0721';'MEG0731';'MEG0741';'MEG0811';'MEG0821';'MEG0911';'MEG0921';'MEG0931';'MEG0941';'MEG1011';'MEG1021';'MEG1031';'MEG1041';'MEG1111';'MEG1121';'MEG1131';'MEG1141';'MEG1211';'MEG1221';'MEG1231';'MEG1241';'MEG1311';'MEG1321';'MEG1331';'MEG1341';'MEG1411';'MEG1421';'MEG1431';'MEG1441';'MEG1511';'MEG1521';'MEG1531';'MEG1541';'MEG1611';'MEG1621';'MEG1631';'MEG1641';'MEG1711';'MEG1721';'MEG1731';'MEG1741';'MEG1811';'MEG1821';'MEG1831';'MEG1841';'MEG1911';'MEG1921';'MEG1931';'MEG1941';'MEG2011';'MEG2021';'MEG2031';'MEG2041';'MEG2111';'MEG2121';'MEG2131';'MEG2141';'MEG2211';'MEG2221';'MEG2231';'MEG2241';'MEG2311';'MEG2321';'MEG2331';'MEG2341';'MEG2411';'MEG2421';'MEG2431';'MEG2441';'MEG2511';'MEG2521';'MEG2531';'MEG2541';'MEG2611';'MEG2621';'MEG2631';'MEG2641'};
+            %cfg.interpolation='linear';
+            cfg.xlim=[1,1];
+            cfg.zlim=[-max(abs(topo.avg)) max(abs(topo.avg))];
+            figure;
+            ft_topoplotER(cfg,topo);
+            title('TOPOGRAPHY OF R')
+        end
     else
         warning('no topoplot without labels and layout fields! see figOptions options')
     end
 end
-
 topoTrace=median(data(:,Ipeaks),2)'*data;
 topoTrace=myFilt(topoTrace,BandPassFilt);
-%dataFilt=myFilt(data,BandPassFilt);
 topoTrace=topoTrace-median(topoTrace);
 topoTraceN=topoTrace./max(topoTrace(1:round(sRate*10)));
 meanMEGN=meanMEGf./max(meanMEGf(1:round(sRate*10)));
@@ -170,24 +189,13 @@ end
 IpeaksR=Ipeaks(r>0.5);
 IpeaksR=IpeaksR(IpeaksR>sampBefore);
 IpeaksR=IpeaksR(IpeaksR<(size(data,2)-sampBefore));
-
-[temp1e,period1]=makeTempHB(mean(data),sRate,IpeaksR,minPeriod/0.6,sampBefore,figs);
+[temp1e,period1]=makeTempHB(meanMEG,sRate,IpeaksR,minPeriod/0.6,sampBefore,figs);
 %% find xcorr between template and meanMEG
 [xcr,lags]=xcorr(meanMEGf,temp1e);
-% figure;
-% plot(lags,xcr);
 xcr=xcr(lags>=0);
-lags=lags(lags>=0);
 [~,tempMax]=max(temp1e);
-%xr(length(t)+1:end)=x(1:end-length(t));
-
 xcrPad=zeros(size(meanMEGf));
-
 xcrPad(tempMax:end)=xcr(1:end-tempMax+1);
-% figure;
-% plot(meanMEGN);
-% hold on
-% plot(xcrPad/max(xcrPad),'r');
 if figs
     figure;
     plot(topoTraceN)
@@ -222,7 +230,6 @@ else
         diary off
     end
     nearI=find(periodS/median(periodS)<0.5);
-    
     if ~isempty(nearI)
         nearT=Ipeaks2(nearI)/sRate; % this is far time, not what you think
         diary('HBlog.txt')
@@ -231,15 +238,11 @@ else
         diary off
     end
 end
-
 % ignore edges
 Ipeaks2in=Ipeaks2(Ipeaks2>sampBefore);
 Ipeaks2in=Ipeaks2in(Ipeaks2in<(size(data,2)-sampBefore));
-% make ecg trace for meanMEG
+% make mcg trace for meanMEG
 [temp2e,period2]=makeTempHB(meanMEG,sRate,Ipeaks2in,period1,sampBefore,figs);
-% FIXME reject bad SNR HB from averaging
-
-
 
 %% test R amplitude
 % meanMEGdt=detrend(meanMEG,'linear',round(sRate:sRate:length(meanMEG)));
@@ -247,91 +250,87 @@ Ipeaks2in=Ipeaks2in(Ipeaks2in<(size(data,2)-sampBefore));
 bef=find(flipLR(temp2e(1:maxi))<0,1)-1;
 aft=find(temp2e(maxi:end)<0,1)-1;
 Rlims=[maxi-bef,maxi+aft]; % check where R pulls the template above zero
-
-% ampMM=meanMEGdt(Ipeaks2in)./temp2e(maxi);
-% mmSm=smooth(meanMEGdt,10);
-% tSm=smooth(temp2e,10);
-% ampMMsm=mmSm(Ipeaks2in)./tSm(maxi);
-
 for HBi=1:length(Ipeaks2in);
     s0=Ipeaks2in(HBi)-bef;
     s1=Ipeaks2in(HBi)+aft;
-    p(HBi,1:2)=polyfit(temp2e(Rlims(1):Rlims(2)),meanMEGdt(s0:s1),1);
+    x=temp2e(Rlims(1):Rlims(2));
+    y=meanMEG(s0:s1);
+    scalef=-round(log10(max([x,y]))); % scaling factor for polyfit not to complain
+    x=x*10^scalef;
+    y=y*10^scalef;
+    pScaled=polyfit(x,y,1);
+    pScaled(2)=pScaled(2).*10^-scalef;
+    p(HBi,1:2)=pScaled; %#ok<AGROW>
 end
 negp=find(p(:,1)<0);
 if ~isempty(negp)
     p(negp,1:2)=0;
 end
 ampMMfit=p(:,1)+(p(:,2)./temp2e(maxi));
+MCG=makeMCG(temp2e,Rlims,Ipeaks2in,ampMMfit,length(meanMEG));
 
-ECG=makeECG(temp2e,Rlims,Ipeaks2in,ampMMfit,length(meanMEG));
-% figure;
-% plot(meanMEGdt,'r')
-% hold on
-% plot(ECG)
-% legend('detrended meanMEG','line fit amp ECG')
-
-
-%% remove ecg from each chan
-
-
+%% remove mcg from each chan
 %make temp per chan
 sBef=maxi-1;
 sAft=length(temp2e)-maxi;
-HBtemp=HBbyChan(data,sRate,Ipeaks2in,period2,sBef,sAft);
+HBtemp=HBbyChan(data,sRate,Ipeaks2in,sBef,sAft);
 % check SNR per chan
 sm50=round(sRate*0.05);
 s0=maxi-sm50*3;
 s1=maxi-sm50;
 s2=maxi+sm50;
-%HBsnr=HBtemp(:,maxi)./max(abs(HBtemp(:,1:round(sBef/2))),[],2);
-n=std(HBtemp(:,s0:s1)');
+n=std(HBtemp(:,s0:s1)'); %#ok<*UDIM>
 s=std(HBtemp(:,s1:s2)');
 snr=s./n;
-badSNR=find(snr<=2);
+badSNR=find(snr<=snrThr);
 if figs
     figure;plot(HBtemp','k');hold on;
     %plot(maxi,HBtemp(find(HBsnr>1.1),maxi),'g.')
     [~,minI]=min(snr);
     plot(HBtemp(minI,:),'r');
-    if isempty(badSNR)
+    if isempty(badSNR) || snrThr==0
         title('red trace is worst SNR channel')
     else
         plot(maxi,HBtemp(badSNR,maxi),'r.')
-        title('red dots mark channels with SNR < 2')
+        title(['red dots mark channels with SNR < ',num2str(snrThr)])
     end
 end
-
 if ~isempty(badSNR)
-    if length(find(snr<=2))>size(data,1)/2
-        error('too many channels have poor SNR')
+    if length(find(snr<=snrThr))>size(data,1)/2
+        error('too many channels have poor SNR. was there artifact?')
     end
-    HBtemp(badSNR,:)=0;
-    diary('HBlog.txt')
-    disp(['not including bad SNR channels, index no. ',num2str(badSNR)])
-    diary off
+    if ~snrThr==0
+        HBtemp(badSNR,:)=0;
+        diary('HBlog.txt')
+        disp(['not including bad SNR channels, index no. ',num2str(badSNR)])
+        diary off
+    end
 end
-% FIXME check poor SNR chans on chan temp
-ECGall=makeECGbyCh(HBtemp,Rlims,Ipeaks2in,ampMMfit,length(meanMEG),maxi);
-
-cleanData=data-ECGall;
+MCGall=makeMCGbyCh(HBtemp,Rlims,Ipeaks2in,ampMMfit,length(meanMEG),maxi);
+cleanData=data-MCGall;
 figure;
-plot(ECG,'k')
+plot(MCG,'k')
 hold on
 plot(meanMEG,'r')
 plot(mean(cleanData),'g')
-legend('ECG from template', 'mean MEG','mean clean MEG')
+legend('MCG from template', 'mean MEG','mean clean MEG')
 Rtopo=HBtemp(:,maxi);
 if figs
-    topo.avg=Rtopo;%median(dataFilt.trial{1,1}(:,Ipeaks),2);
-    cfg.zlim=[-max(abs(Rtopo)) max(abs(Rtopo))];
-    figure;
-    ft_topoplotER(cfg,topo);
-    title ('TOPOGRAPHY OF R')
+    topo.avg=Rtopo;
+    cfg.xlim=[1,1];
+    if strcmp(cfg.layout,'neuromag306mag.lay')
+        cfg.zlim=[-max(abs(topo.avg(magi))) max(abs(topo.avg(magi)))];
+        figure;
+        ft_topoplotER(cfg,topo);
+        title('MAGNETOMETERS, TOPOGRAPHY OF R, 2nd sweep')
+    else
+        cfg.zlim=[-max(abs(Rtopo)) max(abs(Rtopo))];
+        figure;
+        ft_topoplotER(cfg,topo);
+        title ('TOPOGRAPHY OF R, 2nd sweep')
+    end
 end
-
-% FIXME check missing / extra HB
-
+display(['HB period (2nd sweep) is ',num2str(period2),'s']);
 function [tempe,period2]=makeTempHB(trace,sRate,peakIndex,period,sampBefore,figs)
 betweenHBs=0.7; % after the T wave, before next qrs, 0.7 of period
 HB=zeros(size(trace,1),sampBefore*2+1);
@@ -339,10 +338,8 @@ for epochi=1:length(peakIndex)
     HB=HB+trace(:,peakIndex(epochi)-sampBefore:peakIndex(epochi)+sampBefore);
 end
 HB=HB/epochi;
-period2=[];
-
+period2=[]; %#ok<NASGU>
 HBxc=xcorr(HB);
-% FIXME admit defeat for yoni
 [~,ipxc]=findPeaks(HBxc,1.5,sRate*period*0.6); % index peak xcorr
 if length(ipxc)>1
     nextPeak=ceil(length(ipxc)/2+0.5);
@@ -353,13 +350,10 @@ else
     
     period2=median(diff(ipxc))/sRate;
 end
-
-display(['HB period (2nd sweep) is ',num2str(period2),'s']);
-
 temp=HB(sampBefore-round(sRate*(1-betweenHBs)*period2):sampBefore+round(sRate*betweenHBs*period2));
 edgeRepressor=ones(size(temp));
 ms20=round(sRate/50);
-reducVec=[0:1/ms20:1];
+reducVec=0:1/ms20:1;
 reducVec=reducVec(1:end-1);
 edgeRepressor(1:length(reducVec))=reducVec;
 edgeRepressor(end-length(reducVec)+1:end)=flipLR(reducVec);
@@ -372,21 +366,16 @@ if figs
 end
 tempe=double(tempe);
 
-function ECG=makeECG(temp,Rlims,Ipeaks,amp,lengt)
-ECG=zeros(1,lengt);
+function MCG=makeMCG(temp,Rlims,Ipeaks,amp,lengt)
+MCG=zeros(1,lengt);
 [~,maxTemp]=max(temp(1:round(length(temp/2))));
-% bef=find(flipLR(temp(1:maxTemp))<0,1)-1;
-% aft=find(temp(maxTemp:end)<0,1)-1;
-% Rlims=[maxTemp-bef,maxTemp+aft];
-
 for HBi=1:length(Ipeaks);
     s0=Ipeaks(HBi)-maxTemp+1;
     s1=Ipeaks(HBi)+length(temp)-maxTemp;
-    ECG(s0:s1)=temp;
-    ECG(s0+Rlims(1)-1:s0+Rlims(2)-1)=temp(Rlims(1):Rlims(2))*amp(HBi);
+    MCG(s0:s1)=temp;
+    MCG(s0+Rlims(1)-1:s0+Rlims(2)-1)=temp(Rlims(1):Rlims(2))*amp(HBi);
 end
-function tempe=HBbyChan(trace,sRate,peakIndex,period,sampBefore,sampAfter)
-betweenHBs=0.7; % after the T wave, before next qrs, 0.7 of period
+function tempe=HBbyChan(trace,sRate,peakIndex,sampBefore,sampAfter)
 HB=zeros(size(trace,1),sampBefore+1+sampAfter);
 % average HBs
 for epochi=1:length(peakIndex)
@@ -396,18 +385,18 @@ HB=HB/epochi;
 % reduce edges to vero
 edgeRepressor=ones(1,size(HB,2));
 ms20=round(sRate/50);
-reducVec=[0:1/ms20:1];
+reducVec=0:1/ms20:1;
 reducVec=reducVec(1:end-1);
 edgeRepressor(1:length(reducVec))=reducVec;
 edgeRepressor(end-length(reducVec)+1:end)=flipLR(reducVec);
 tempe=HB-repmat(mean(HB(:,[1:ms20,end-ms20:end]),2),1,size(HB,2));
 tempe=tempe.*repmat(edgeRepressor,size(HB,1),1);
-function ECG=makeECGbyCh(temp,Rlims,Ipeaks,amp,lengt,maxTemp)
-ECG=zeros(size(temp,1),lengt);
+function MCG=makeMCGbyCh(temp,Rlims,Ipeaks,amp,lengt,maxTemp)
+MCG=zeros(size(temp,1),lengt);
 for HBi=1:length(Ipeaks);
     s0=Ipeaks(HBi)-maxTemp+1;
     s1=Ipeaks(HBi)+length(temp)-maxTemp;
-    ECG(:,s0:s1)=temp;
-    ECG(:,s0+Rlims(1)-1:s0+Rlims(2)-1)=temp(:,Rlims(1):Rlims(2))*amp(HBi);
+    MCG(:,s0:s1)=temp;
+    MCG(:,s0+Rlims(1)-1:s0+Rlims(2)-1)=temp(:,Rlims(1):Rlims(2))*amp(HBi);
 end
 
