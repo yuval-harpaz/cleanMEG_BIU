4aab1287bcc99d67c8bbce1c9c8599cd59ee3055
yuval <yuval@bar-ilan>
2013-12-10 14:17:20 +0200

    neurromag example

---
+++ correctHB_neuromag_example.m
@@ -0,0 +1,20 @@
+% Example of cleaning neuromag data of HeartBeat artifact
+% I read the fif file with fieldtrip
+
+LS=ls('*.fif');
+LS=LS(1:end-1);
+hdr=ft_read_header(LS);
+trl=[1,hdr.nSamples,0];
+cfg=[];
+cfg.trl=trl;
+cfg.demean='yes';
+cfg.dataset=LS;
+cfg.channel='MEGMAG';
+mag=ft_preprocessing(cfg);
+meanMAG=mean(mag.trial{1,1});
+cfg.channel='MEG';
+meg=ft_preprocessing(cfg);
+figOptions.label=meg.label;
+figOptions.layout='neuromag306mag.lay';
+clear mag
+[cleanMEG,tempMEG,periodMEG,mcgMEG,RtopoMEG]=correctHB(meg.trial{1,1},meg.fsample,figOptions,meanMAG);
