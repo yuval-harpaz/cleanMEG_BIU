4f4455137913a21990070e32b53c38c406cad0c7
yuval <yuval@bar-ilan>
2013-12-16 15:40:24 +0200

    4D example update

--- correctHB_4D_example.m
+++ correctHB_4D_example.m
@@ -18,19 +18,9 @@ fileName = 'c,rfhp0.1Hz';
 p=pdf4D(fileName);
 cleanCoefs = createCleanFile(p, fileName,'byLF',256 ,'Method','Adaptive','xClean',[4,5,6],'byFFT',0,'HeartBeat',0);
 % then you get a file called 'xc,lf_c,rfhp0.1Hz' or something similar
-% then clean the heartbeat like this
-fn='xc,lf_c,rfhp0.1Hz';
-p=pdf4D(fn);
-sRate=double(get(p,'dr'));
-hdr = get(p, 'header');
-nSamp=hdr.epoch_data{1,1}.pts_in_epoch;
-chi = channel_index(p, 'meg', 'name');
-data = read_data_block(p,[1 nSamp],chi);
-data=double(data);
-label=channel_label(p,chi)';
-figOptions.label=label;
-figOptions.layout='4D248.lay';
-[cleanData,temp2e,period2,MCG,Rtopo]=correctHB(data,sRate,figOptions);
-% or without the figOptions if you don't like them all over the screen
+% then clean the heartbeat like this (it lookx for xc,lf_* file)
+newData=correctHB; % or newData=correctHB([],[],1); if you want lots of plots
+rewrite_pdf(newData);
+
 
 
