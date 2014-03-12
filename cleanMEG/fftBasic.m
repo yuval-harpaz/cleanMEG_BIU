cf28ed0509a9e7eecf39018a091f3270ae423fcb
yuval <yuval@bar-ilan>
2014-03-12 10:45:45 +0200

    one by one push

--- cleanMEG/fftBasic.m
+++ cleanMEG/fftBasic.m
@@ -1,15 +1,17 @@
-function [fourier,freq]=fftBasic(rows,Fs,binSize)
+function [fourier,freq]=fftBasic(rows,Fs)
 % rows = data with rows for channels
 % Fs = Sampling frequency
-% binSize = how many Hz per freq bin, default is one.
-if ~exist('binSize','var')
-    binSize=1;
-elseif isempty(binSize)
-    binSize=1;
-end
+
 L = size(rows,2)/Fs;                     % Length of signal
-NFFT = round(Fs)/binSize; % this gives bins of roughly  1Hz
-Y = fft(rows',NFFT);
+NFFT = round(Fs); % this gives bins of roughly  1Hz
+for seci=1:NFFT:(size(rows,2)-NFFT)
+    if seci==1
+        Y = fft(rows',NFFT);
+    else
+        Y = Y+fft(rows(:,seci:seci+NFFT)',NFFT);
+    end
+end
+Y=Y./length(1:NFFT:(size(rows,2)-NFFT)); % average over seconds
 fourier=Y(1:floor(NFFT/2)+1,:);
 freq = Fs/2*linspace(0,1,NFFT/2+1);
 fourier=fourier';
