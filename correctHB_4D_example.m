eda36e94d7648d1b94750e555536e7022ed8903c
yuval-harpaz <yuvharpaz@gmail.com>
2014-12-15 14:56:31 +0200

    Update correctHB_4D_example.m

--- correctHB_4D_example.m
+++ correctHB_4D_example.m
@@ -6,19 +6,17 @@
 newData=correctHB;
 % write it to a new file
 rewrite_pdf(newData);
-% rename
-fn=ls('rw_*');
-fn=fn(4:end-1);
-eval(['!mv rw_',fn,' hb_',fn])
+% rename the rw_* file if you want to
+
 
 %% BIU users
-% better clean accelerometers first if you have them 
+% better clean line frequency and accelerometers first
 % use createCleanFile with 'HeartBeat',0, like this
 fileName = 'c,rfhp0.1Hz';
 p=pdf4D(fileName);
 cleanCoefs = createCleanFile(p, fileName,'byLF',256 ,'Method','Adaptive','xClean',[4,5,6],'byFFT',0,'HeartBeat',0);
 % then you get a file called 'xc,lf_c,rfhp0.1Hz' or something similar
-% then clean the heartbeat like this (it lookx for xc,lf_* file)
+% then clean the heartbeat like this (it looks for xc,lf_* file)
 newData=correctHB; % or newData=correctHB([],[],1); if you want lots of plots
 rewrite_pdf(newData); % or rewrite_pdf(newData,[],[],'xc,lf,hb') if you want standard BIU naming
 
